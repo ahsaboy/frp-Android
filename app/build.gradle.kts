c6a138a352bf9f5c6e14d4b57c990e226a7fbcc1
ahsaboy <1664129167@qq.com>
2025-12-06 23:31:05 +0800

    支持导入/导出配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         minSdk = 23
         targetSdk = 35
         compileSdk = 35
-        versionCode = 19
-        versionName = "1.5.2"
+        versionCode = 20
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
