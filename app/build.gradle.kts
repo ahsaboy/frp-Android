96b1d42b58f831689582498c70003a28a48a95bf
ahsaboy <1664129167@qq.com>
2026-05-02 16:11:11 +0800

    fix(Tasker): tasker配置页面主题适配miui主题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         minSdk = 23
         targetSdk = 37
         compileSdk = 37
-        versionCode = 24
-        versionName = "1.5.7"
+        versionCode = 25
+        versionName = "1.5.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
